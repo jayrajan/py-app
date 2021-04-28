20e386fe7d548228620493b4d66cbfe76a47f7ea
jayrajan <jerin.rajan@kcl.ac.uk>
2021-04-28 09:04:03 +0100

    update: minor

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #  Code written by Jerin Rajan on 19th april 2020
+
+print('hello py-world\n')
+
+
+# Core function:
+# Create a database
+#
+# Create new item in database
+# Read from database
+# Update the database
+# Delete a record
