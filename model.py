c9c8b727d6529a149c46fb51ea3a1e350b17a322
jayrajan <jerin.rajan@kcl.ac.uk>
2021-05-05 08:41:57 +0100

    feat: sqlite3 db with con and cur object, closing the db connection

--- model.py
+++ model.py
@@ -1,7 +1,28 @@
+import sqlite3
+
 # Model Code
 
 # Backend:
 # ========
 # Handling the data
 
+
+# Declarations
+db_name = 'TimeBooking.db'
+
+
 # Create a Database
+
+# Check to see if the database already exists
+
+
+# Create a connection object
+con = sqlite3.connect(db_name)
+
+
+
+
+# cursor object
+cur = con.cursor()
+# Close the connection
+con.close()
